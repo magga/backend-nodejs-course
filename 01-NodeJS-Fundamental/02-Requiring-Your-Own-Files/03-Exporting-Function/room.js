bcf59492e6f9123c574fff4dab4e5563bddbe309
Magga Panna <magga.panna@live.com>
2017-07-20 11:23:28 +0700

    Add Arrow Function in Section 1.2 File 3

--- 01-NodeJS-Fundamental/02-Requiring-Your-Own-Files/03-Exporting-Function/room.js
+++ 01-NodeJS-Fundamental/02-Requiring-Your-Own-Files/03-Exporting-Function/room.js
@@ -1,6 +1,13 @@
 console.log("Starting Room.js");
 
-module.exports.addWeather = function() {
+// // Using regular syntax
+// module.exports.addWeather = function() {
+//     console.log("Adding Weather");
+//     return "New Weather";
+// }
+
+// Using ES6 Arrow Function
+module.exports.addWeather = () => {
     console.log("Adding Weather");
     return "New Weather";
 }
