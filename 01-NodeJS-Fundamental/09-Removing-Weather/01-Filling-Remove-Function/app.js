a73a08bced51dec1c3f875fea51c7e656992d18e
Magga Panna <magga.panna@live.com>
2017-07-20 16:21:14 +0700

    Add Section 1.9 File 1

---
+++ 01-NodeJS-Fundamental/09-Removing-Weather/01-Filling-Remove-Function/app.js
@@ -0,0 +1,25 @@
+const yargs = require("yargs");
+const yArgv = yargs.argv;
+
+const room = require("./room.js");
+
+var command = yArgv._[0];
+
+if (command === "add") {
+    var weather = room.addWeather(yArgv.tem, yArgv.hum);
+    console.log(`Add Temperature : ${weather.temperature}, Humidity : ${weather.humidity}, Timestamp : ${weather.timestamp}`);
+} else if (command === "list") {
+    room.getAll();
+} else if (command === "read") {
+    room.getWeather(yArgv.time);
+} else if (command === "remove") {
+    var deleted = room.removeWeather(yArgv.time);
+
+    if (deleted) {
+        console.log("Weather successfully deleted!");
+    } else {
+        console.log("No weather found");
+    }
+} else {
+    console.log("Command Not Recognized");
+} 
