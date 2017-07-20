f39c7858b9e75655dc1f5418d7045bd089917acc
Magga Panna <magga.panna@live.com>
2017-07-20 11:28:40 +0700

    Add Section 1.2 File 4

---
+++ 01-NodeJS-Fundamental/02-Requiring-Your-Own-Files/04-C-Function-With-Parameter/app.js
@@ -0,0 +1,9 @@
+console.log("Starting App.js");
+
+const room = require("./room.js");
+
+// var result = room.addWeather();
+// console.log(result);
+
+var result = room.add(6, 8);
+console.log(result);
---
+++ 01-NodeJS-Fundamental/02-Requiring-Your-Own-Files/04-C-Function-With-Parameter/room.js
@@ -0,0 +1,10 @@
+console.log("Starting Room.js");
+
+module.exports.addWeather = () => {
+    console.log("Adding Weather");
+    return "New Weather";
+}
+
+module.exports.add = (angka1, angka2) => {
+    return angka1 + angka2;
+}
