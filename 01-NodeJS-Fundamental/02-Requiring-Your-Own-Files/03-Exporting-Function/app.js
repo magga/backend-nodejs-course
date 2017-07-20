1ed720b8b0dac3489f5142a745b8d35302f534ee
Magga Panna <magga.panna@live.com>
2017-07-20 11:16:01 +0700

    Add Section 1.2 File 3

---
+++ 01-NodeJS-Fundamental/02-Requiring-Your-Own-Files/03-Exporting-Function/app.js
@@ -0,0 +1,7 @@
+console.log("Starting App.js");
+
+const room = require("./room.js");
+
+var result = room.addWeather();
+
+console.log(result);
---
+++ 01-NodeJS-Fundamental/02-Requiring-Your-Own-Files/03-Exporting-Function/room.js
@@ -0,0 +1,6 @@
+console.log("Starting Room.js");
+
+module.exports.addWeather = function() {
+    console.log("Adding Weather");
+    return "New Weather";
+}
